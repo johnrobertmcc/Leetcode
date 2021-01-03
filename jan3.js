be30f0008e529e940eb3f01b865b5ff4f5ab0be1
johnrobertmcc <john.robert.mcc@gmail.com>
2021-01-03 17:54:34 -0500

    goal parser

--- jan3.js
+++ jan3.js
@@ -1,6 +1,29 @@
-var interpret = function(command) {
-    console.log(command)
+var maximumWealth = function(accounts) {
+    
+    let max= 0;
+    
+    for(let i=0; i< accounts.length; i++){
+        let current = accounts[i];
+        let temp = 0;
+        for(let j=0; j<current.length; j++){
+            temp += current[j];     
+        }
+        if(temp > max){
+            max = temp;
+        }
+    }
+    
+    return max;
     
 };
 
+
+var interpret = function(command) {
+    let temp = command.replace(/\(al\)/gi, 'al')
+    let fin = temp.replace(/\(\)/gi, 'o');
+    console.log(fin)
+};
+
 interpret("(al)G(al)()()G")
+interpret("G()()()()(al)")
+interpret("G()(al)")
