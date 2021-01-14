fe67903757ed9803464ed14592bbc5a81db7c90c
johnrobertmcc <john.robert.mcc@gmail.com>
2021-01-14 16:34:49 -0500

    intersect

--- jan14.js
+++ jan14.js
@@ -16,10 +16,36 @@ var sortColors = function(nums) {
     return nums;
 };
 
-console.log(sortColors([2,0,2,1,1,0]))
+// console.log(sortColors([2,0,2,1,1,0]))
+
+const find = function(arr, value){
+
+    return arr.findIndex(num => {
+        return num == value
+    })
+}
+
+const check = function(a, b) {
+    if(a == b){
+        return true
+    }
+
+    return false
+}
 
 var intersect = function(nums1, nums2) {
+
+    for(let i = 0;i < nums1.length; i++){
+
+            let start = nums1[i];
+            let second = find(nums2, start);
+            let temp = nums2.slice(second)
+            
+            if(check(temp[1], nums1[i+1])){
+                return [nums1[i], nums1[i+1]]
+            }
+    }
     
 };
 
-console.log(intersect([4,9,5], [9,4,9,8,4]))
+console.log(intersect([5,4,9,5], [9,4,9,8,4]))
