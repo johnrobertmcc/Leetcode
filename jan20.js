685cf3cf730e3bda691a78a70f6bade239067c97
johnrobertmcc <john.robert.mcc@gmail.com>
2021-01-20 16:03:26 -0500

    missingNumber

--- jan20.js
+++ jan20.js
@@ -1,3 +1,19 @@
 var missingNumber = function(nums) {
+    let max = Math.max(...nums);
+    let sum = 0;
+    let numsSum = 0;
+
+    for(let i = 0; i <= max; i++){
+        sum += i
+    }
+
+    for(let i = 0; i < nums.length; i++){
+        numsSum += nums[i]
+    }
+
+    return sum - numsSum
+
     
 };
+
+// console.log(missingNumber([3,0,1]))
