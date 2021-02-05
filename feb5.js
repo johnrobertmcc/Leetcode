2e0cc1558e52c359df524d0c536536ed406d1b9b
johnrobertmcc <john.robert.mcc@gmail.com>
2021-02-05 16:45:48 -0500

    it wrks fuck leetcode

--- feb5.js
+++ feb5.js
@@ -4,33 +4,46 @@ var getSum = function(a, b) {
     let bottom;
     let fin = [];
     let negative = false;
+    let half = false;
+    let temp = [];
     
     top =  a > b ? a : b;
     bottom = a > b ? b : a;
     
-    if(top < 0){
+    if(top < 0 && bottom < 0){
         negative = true;
+        top *= -1;
+        bottom *= -1;
+    }else if(bottom < 0 && top > 0){
+        half = true;
+        if(bottom*-1 === top){
+            return 0
+        }
+    }
+    
+    for(let i = 0; i < top; i++){
+        fin.push(i);
     }
 
-    for(let i = top; i > 0; i--){
-        fin.push(i);
-     }
-    
-    if(bottom < 0 && !negative){
-        fin.slice(bottom*-1)
+    if(half){
+        for(let i = bottom; i < 0; i++){
+            temp.push(i);
+        }
+
+        return fin.slice(temp.length).length
     }else{
-     for(let i = 0; i < bottom; i++){
-        fin.push(i);
-     }
+        for(let i = 0; i < bottom; i++){
+            fin.push(i)
+        }
     }
-    
-    
+    //    return [top, bottom, fin, temp, half]
     return negative ? fin.length*-1 : fin.length
+    
 
     
 };
 
-console.log(getSum(-1, -1))
+console.log(getSum(-14, 16))
 
 
 // top = 1
